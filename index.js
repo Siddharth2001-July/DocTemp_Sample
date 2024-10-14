e7567a3e51f48437c3a7271509a5a02229eef9eb
Siddharth Kamboj <71596674+Siddharth2001-July@users.noreply.github.com>
2024-10-14 18:28:41 +0530

    vanillaJS Sample

--- index.js
+++ index.js
@@ -1,41 +1,22 @@
 const PSPDFKit = window.PSPDFKit;
 
 // We need to inform PSPDFKit where to look for its library assets, i.e. the location of the `pspdfkit-lib` directory.
-const baseUrl = 'https://cdn.cloud.pspdfkit.com/pspdfkit-web@2024.4.0/';
+const baseUrl = "https://cdn.cloud.pspdfkit.com/pspdfkit-web@2024.4.0/";
 
 var _instance = null;
 
-async function runner(){
-  let data = {
-    config: {
-      delimiter: {
-        start: "{{",
-        end: "}}"
-      }
-    },
-    model: {
-      helloWorld: "PSPDFKit" 
-    }
-  }
-  const docBuffer = await PSPDFKit.populateDocumentTemplate(
-    {
-      document: "doctemp.docx"
-    },
-    data
-  )
+async function runner() {
   await PSPDFKit.load({
     baseUrl,
     container: "#pspdfkit",
-    document: docBuffer,
+    document: "document.pdf",
     toolbarItems: [...PSPDFKit.defaultToolbarItems, { type: "comment" }],
   })
-    .then((instance) => {
+    .then(async (instance) => {
       _instance = instance;
     })
     .catch((error) => {
       console.error(error.message);
     });
-
 }
 runner();
-
